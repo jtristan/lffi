ca84c43ea352b6f23085aef19ba4fb906524a5a3
jtristan <trjohnb@amazon.com>
2024-07-10 14:51:53 -0400

    Working example

--- lakefile.lean
+++ lakefile.lean
@@ -1,5 +1,7 @@
 import Lake
+
 open Lake DSL
+open System IO FS
 
 package lffi where
   -- add package configuration options here
@@ -10,3 +12,14 @@ lean_lib «Lffi» where
 @[default_target]
 lean_exe lffi where
   root := `Main
+
+target ffi.o pkg : FilePath := do
+  let oFile := pkg.buildDir / "ffi.o"
+  let srcJob ← inputTextFile <| pkg.dir / "ffi.cpp"
+  let weakArgs := #["-I", (← getLeanIncludeDir).toString]
+  buildO oFile srcJob weakArgs #["-fPIC"] "c++" getLeanTrace
+
+extern_lib libleanffi pkg := do
+  let ffiO ← ffi.o.fetch
+  let name := nameToStaticLib "leanffi"
+  buildStaticLib (pkg.nativeLibDir / name) #[ffiO]
